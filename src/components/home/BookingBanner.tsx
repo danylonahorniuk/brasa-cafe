9a4f94aac7c18683cb5fbe8769c3fd9544b63174
danylonahorniuk <danylo.nahorniuk@gmail.com>
2026-06-03 22:10:48 +0300

    home: replace duplicate photo in booking banner

--- src/components/home/BookingBanner.tsx
+++ src/components/home/BookingBanner.tsx
@@ -12,7 +12,7 @@ export default function BookingBanner() {
         {/* Фото */}
         <div className="relative min-h-[280px] md:min-h-0">
           <Image
-            src="https://images.unsplash.com/photo-1414235077428-338989a2e8c0?w=900&q=80"
+            src="https://images.unsplash.com/photo-1552566626-52f8b828add9?w=900&q=80"
             alt="Зал Brasa"
             fill
             className="object-cover"
